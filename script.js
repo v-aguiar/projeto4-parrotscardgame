bb28ffe33f761b704826a5186797e29761f3b11d
v-aguiar <v.aguiar.dev@gmail.com>
2022-02-07 11:37:17 -0300

    Prevent a few bugs when player clicks the same card repeatedly

--- script.js
+++ script.js
@@ -1,176 +1,167 @@
+// Declarations
+let youWinIterator = 0
+let turnedCardsCount = 0
+let clicks = 0
+
 // Card Functions
-function addCards() {
-  const cardsContainer = document.querySelector(".cards-container")
+const cards = {
+  cardsContainer: document.querySelector(".cards-container"),
+  clickedCards: [],
 
-  cardsContainer.innerHTML = createCardsRandomizedStructure()
-}
+  getNumberOfCards() {
+    const numberOfCards = parseInt(prompt("Digite um número par entre 4 e 14 para jogar", "Ex.: '4', '6', '12'"))
 
-function createCardsRandomizedStructure() {
-  const arrayOfNumbers = []
+    const validateEvenNumber = (numberOfCards % 2 == 0)
+    const validateNumberOfCards = ((numberOfCards >= 4) && (numberOfCards <= 14))
+    const isNotValid = ((!validateEvenNumber === false) && (!validateNumberOfCards === false))
 
-  let bgNumber = null
-  let cardStructure = ''
-
-  for(let index = 0; index < validNumberOfCards; index++) {
-    bgNumber = getRandomNumber(validNumberOfCards / 2)
-
-    let maxRandomBgArray = (arrayOfNumbers.filter((number) => number === bgNumber))
-
-    // Verifies if the same background has already been aplied two times
-    while(maxRandomBgArray.length === 2) {
-      bgNumber = getRandomNumber(validNumberOfCards / 2)
-
-      maxRandomBgArray = (arrayOfNumbers.filter((number) => number === bgNumber))
+    if(isNotValid !== true) {
+      this.getNumberOfCards();
+    } else {
+      return numberOfCards;
     }
+  },
 
-    cardStructure += `
-      <div class="card" data-identifier="card">
-      <div class="front-face face" data-identifier="front-face"></div>
-      <div class="back-face face bg${bgNumber}" data-identifier="back-face"></div>
-      </div>
-      `
+  addCards() {
+    this.cardsContainer.innerHTML = this.createCardsRandomizedStructure()
+  },
 
-    arrayOfNumbers.push(bgNumber)
-  }
-  return cardStructure
-}
+  createCardsRandomizedStructure() {
+    const bgNumbers = []
 
-function areAllCardsFacedUp() {
-  if(youWinComparator === validNumberOfCards) {
-    timer.stopTimer()
+    let bgNumber = null
+    let cardStructure = ''
 
-    setTimeout(() => {
-      alert(`Você ganhou em ${timer.element.innerHTML} segundos e ${turnedCardsCount} jogadas!`)
-    }, 500)
+    for(let index = 0; index < validNumberOfCards; index++) {
+      do {
+        bgNumber = getRandomNumber(validNumberOfCards / 2)
 
-    setTimeout(() => {
-      const answer = prompt('Deseja testar sua memória outra vez? Y/N')
-      const validAnswer = answer.trim().toUpperCase()
+        // Verifies if the same background has already been aplied two times
+        sameRandomBgs = (bgNumbers.filter((number) => number === bgNumber))
+      } while(sameRandomBgs.length === 2)
 
-      if(validAnswer === 'Y' || validAnswer === 'YES') {
-        restartGame()
-      }
-    }, 500)
-  }
-}
+      cardStructure += `
+        <div class="card" data-identifier="card">
+          <div class="front-face face" data-identifier="front-face"></div>
+          <div class="back-face face bg${bgNumber}" data-identifier="back-face"></div>
+        </div>
+        `
 
-// Card click functions
-function handleCardClick() {
-  const cards = document.querySelectorAll('.card')
+      // stores applied background number 
+      bgNumbers.push(bgNumber)
+    }
+    console.log("Randomized, returning HTML whith all cards")
+    return cardStructure
+  },
 
-  cards.forEach((card) => {
-    card.addEventListener('click', () => {
-      clickedCards.push(card)
-      turnedCardsCount++
-      clicks++
+  // Card click functions
+  handleCardClick() {
+    const allCards = document.querySelectorAll(".card")
 
-      // Start counting time
-      if(turnedCardsCount === 1) {
-        timer.startTimer()
-      }
+    allCards.forEach((card) => {
+      card.addEventListener('click', () => {
+        this.clickedCards.push(card)
+        turnedCardsCount++
+        clicks++
 
-      const firstClick = (clicks === 1)
-      const secondClick = (clicks === 2)
-      const offClick = (clicks >= 3)
+        // Start counting time
+        if(turnedCardsCount === 1) {
+          timer.startTimer()
+        }
 
-      if(firstClick) {
-        flipClickedCard(card)
-      } else if(secondClick) {
-        flipClickedCard(card)
-        compareClickedCards()
-      } else if(offClick) {
-        turnedCardsCount--
-        clicks--
-      }
+        const firstClick = (clicks === 1)
+        const secondClick = (clicks === 2)
+
+        if(firstClick) {
+          this.flipClickedCard(card)
+        } else if(secondClick) {
+          this.flipClickedCard(card)
+          this.compareClickedCards()
+        } else {
+          turnedCardsCount--
+          clicks--
+        }
+      })
     })
-  })
-}
+  },
 
-function compareClickedCards() {
-  const firstClickedCardClassList = (clickedCards[0].children[1].classList.value)
-  const secondClickedCardClassList = (clickedCards[1].children[1].classList.value)
+  compareClickedCards() {
+    const firstClickedCardClassList = (this.clickedCards[0].children[1].classList.value)
+    const secondClickedCardClassList = (this.clickedCards[1].children[1].classList.value)
+    const firstClickedCardParents = (this.clickedCards[0].children[1].parentNode)
+    const secondClickedCardParents = (this.clickedCards[1].children[1].parentNode)
 
-  const cardsAreEqual = firstClickedCardClassList == secondClickedCardClassList
+    // Make sure clicked cards have same BG but are not the same card
+    const cardsAreEqual = firstClickedCardClassList == secondClickedCardClassList
+    const cardsHaveDiffParents = firstClickedCardParents !== secondClickedCardParents
 
-  if(cardsAreEqual) {
-    youWinComparator += 2
+    if(cardsAreEqual && cardsHaveDiffParents) {
+      youWinIterator += 2
 
-    resetClickCount()
-    areAllCardsFacedUp()
+      this.resetClickCount()
+      this.areAllCardsFacedUp()
 
-  } else {
-    flipClickedCardsBack()
+    } else if(cardsHaveDiffParents) {
+      this.flipClickedCardsBack()
+    }
+    else {
+      clicks--
+      this.clickedCards.pop()
+    }
+  },
+
+  areAllCardsFacedUp() {
+    if(youWinIterator === validNumberOfCards) {
+      timer.stopTimer()
+
+      setTimeout(() => {
+        alert(`Você ganhou em ${timer.element.innerHTML} segundos e ${turnedCardsCount} jogadas!`)
+      }, 500)
+
+      setTimeout(() => {
+        const answer = prompt('Deseja testar sua memória outra vez? Y/N')
+        const validAnswer = answer.trim().toUpperCase()
+
+        if(validAnswer === 'Y' || validAnswer === 'YES') {
+          restartGame()
+        }
+      }, 500)
+    }
+  },
+
+  resetClickCount() {
+    clicks = 0
+
+    console.log('clickeCardsBefore:', this.clickedCards)
+
+    for(let index = 0; index <= this.clickedCards.length; index++) {
+      this.clickedCards.pop()
+    }
+    console.log('clickeCardsAfter:', this.clickedCards)
+  },
+
+  flipClickedCard(card) {
+    // Flip card
+    card.querySelector('.back-face').style.transform = 'rotateY(0deg)'
+    card.querySelector('.front-face').style.transform = 'rotateY(-180deg)'
+  },
+
+  flipClickedCardsBack() {
+    // Return card to initial position
+    this.clickedCards.forEach((card) => {
+      setTimeout(() => {
+        card.querySelector('.back-face').style.transform = 'rotateY(180deg)'
+        card.querySelector('.front-face').style.transform = 'rotateY(0deg)'
+      }, 1000)
+    })
+    this.resetClickCount()
   }
-}
-
-function resetClickCount() {
-  clicks = 0
-
-  for(let index = 0; index <= clickedCards.length; index++) {
-    clickedCards.pop()
-  }
-}
-
-function flipClickedCard(card) {
-  // Flip card
-  card.querySelector('.back-face').style.transform = 'rotateY(0deg)'
-  card.querySelector('.front-face').style.transform = 'rotateY(-180deg)'
-}
-
-function flipClickedCardsBack() {
-  // Return card to initial position
-  clickedCards.forEach((card) => {
-    setTimeout(() => {
-      card.querySelector('.back-face').style.transform = 'rotateY(180deg)'
-      card.querySelector('.front-face').style.transform = 'rotateY(0deg)'
-    }, 1000)
-  })
-  resetClickCount()
-}
-
-// Get valid number of cards
-function getNumberOfCards() {
-  const numberOfCards = parseInt(prompt("Digite um número par entre 4 e 14 para jogar", "Ex.: '4', '6', '12'"))
-
-  const validateEvenNumber = (numberOfCards % 2 == 0)
-  const validateNumberOfCards = ((numberOfCards >= 4) && (numberOfCards <= 14))
-  const isNotValid = ((!validateEvenNumber === false) && (!validateNumberOfCards === false))
-
-  if(isNotValid !== true) {
-    getNumberOfCards();
-  } else {
-    return numberOfCards;
-  }
-}
-
-// Randomizer
-function getRandomNumber(number) {
-  const randomNumber = (Math.floor(Math.random() * number))
-
-  return randomNumber + 1
-}
-
-// Restar game -> Bonus
-function restartGame() {
-  youWinComparator = 0
-  turnedCardsCount = 0
-  resetClickCount()
-
-  timer.element.innerHTML = '0'
-
-  const cardsContainer = document.querySelector('.cards-container')
-  cardsContainer.innerHTML = ''
-
-  validNumberOfCards = getNumberOfCards()
-
-  setTimeout(() => {addCards()}, 500)
-  setTimeout(() => {handleCardClick()}, 500)
 }
 
 // Timer Functions - Bonus (training object usage)
 const timer = {
   element: document.querySelector('header .timer'),
-  interval: number = 0,
+  interval: 0,
 
   startTimer() {
     const interval = setInterval(() => {
@@ -185,15 +176,32 @@ const timer = {
   }
 }
 
-// Declarations
+// Randomizer
+function getRandomNumber(number) {
+  const randomNumber = (Math.floor(Math.random() * number))
 
-let validNumberOfCards = getNumberOfCards()
-const clickedCards = []
+  return randomNumber + 1
+}
 
-let youWinComparator = 0
-let turnedCardsCount = 0
-let clicks = 0
+// Restar game -> Bonus
+function restartGame() {
+  youWinIterator = 0
+  turnedCardsCount = 0
+  cards.resetClickCount()
 
-addCards()
-handleCardClick()
+  timer.element.innerHTML = '0'
+
+  // const cardsContainer = document.querySelector('.cards-container')
+  cards.cardsContainer.innerHTML = ''
+
+  validNumberOfCards = cards.getNumberOfCards()
+
+  setTimeout(() => {cards.addCards()}, 500)
+  setTimeout(() => {cards.handleCardClick()}, 500)
+}
+
+let validNumberOfCards = cards.getNumberOfCards()
+
+cards.addCards()
+cards.handleCardClick()
 
